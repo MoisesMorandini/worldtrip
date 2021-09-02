ab22572793272ea08fd351fecf703b0c50009b94
MoisesMorandini <moises-morandini@hotmail.com>
2021-09-02 15:34:09 -0300

    first commit

---
+++ src/styles/theme.ts
@@ -0,0 +1,24 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    white: '#F5F8FA',
+    gray: {
+      "600": "#47585B",
+      "500": "#999999",
+      "200": "#DADADA",
+    }
+  },
+  fonts: {
+    heading: 'Poppins',
+    body: 'Poppins'
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'white',
+        color: 'white'
+      }
+    }
+  }
+});
