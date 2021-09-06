0b7834a5a6eed576cd31ccc0f9205cc9b005bce1
MoisesMorandini <moises-morandini@hotmail.com>
2021-09-06 15:07:59 -0300

    Added Banner component into home

---
+++ src/components/Home/Banner.tsx
@@ -0,0 +1,22 @@
+import { Stack, Image, Text } from '@chakra-ui/react'
+
+interface BannerProps {
+  isLargarThan1280?: boolean;
+}
+
+export function Banner({ isLargarThan1280 = false }: BannerProps) {
+  return (
+    <Stack position="relative" h={['163px', '250px', '335']} spacing="1.25em" pl={['1em', '3em', '6em', '8.75em']} backgroundImage="url('./images/Background.png')">
+      <Text as="h1" maxWidth="426px" mt={['1.4em', '2em', '2.22em']} color="white"
+        fontWeight="500" fontSize={['20px', '28px', '36px']} lineHeight={['30px', '42px', '54px']}>
+        5 Continentes,<br /> infinitas possibilidades.
+      </Text>
+      <Text as="h2" maxWidth="524px" color="gray.200"
+        fontWeight="400" fontSize={['14px', '17px', '20px']} lineHeight={['21px', '25px', '30px']}>
+        Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+      </Text>
+      {isLargarThan1280 && <Image src="./images/Airplane.png" alt="Airplane" w="417px" h="270px"
+        position="absolute" top='4.75em' right="8.75em" transform={"rotate(3deg)"} />}
+    </Stack>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Stack, Image, Text, useMediaQuery } from '@chakra-ui/react'
+
 import Head from 'next/Head'
 import { Header } from '../components/Header'
+import { Banner } from '../components/Home/Banner';
+
 export default function Home() {
+  const [isLargarThan1280] = useMediaQuery("(min-width: 1280px)");
+
   return (
     <>
       <Head>
@@ -9,7 +14,7 @@ export default function Home() {
       </Head>
       <Flex direction="column" maxWidth="1440px" m="0 auto">
         <Header />
-        <Header backHome />
+        <Banner isLargarThan1280={isLargarThan1280} />
       </Flex>
     </>
   )
